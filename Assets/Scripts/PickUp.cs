f95b8ac2c9a4699d71dfe9a7fd9afff99385a247
Papventje <robbierobsnor@gmail.com>
2017-04-11 14:57:02 +0200

    Hai

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class PickUp : MonoBehaviour {
+
+    [SerializeField]
+    private Text pickUpText;
+    private bool pickedUp = true;
+    private bool hintSound = true;
+    private float waitTime = 10f;
+
+    [SerializeField]
+    private string objectName = "";
+
+    public AudioClip clip;
+
+	// Use this for initialization
+	void Start () {
+        pickUpText.enabled = false;
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            pickUpText.enabled = true;
+        if (pickedUp == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(gameObject);
+            pickUpText.enabled = false;
+            //Inventory.add(objectName);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            pickUpText.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && hintSound)
+        {
+            AudioSource.PlayClipAtPoint(clip, transform.position, 0.01f);
+            StartCoroutine("HintSoundCooldown");
+        }
+    }
+
+    IEnumerator HintSoundCooldown()
+    {
+        hintSound = false;
+        yield return new WaitForSeconds(waitTime);
+        hintSound = true;
+    }
+}
---
+++ Assets/Scripts/RotateTowardsPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.forward = Camera.main.transform.position - this.transform.position;
+	}
+}
